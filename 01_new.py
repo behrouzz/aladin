c35685f9d497296a22537f8a4e4348e665175f18
Behrouz <behrouz.safari@gmail.com>
2022-07-16 12:17:11 +0200

    del template

---
+++ 01_new.py
@@ -0,0 +1,21 @@
+from aladin import Aladin
+
+a = Aladin(target='151.75735684271, -40.43642515362001', fov=0.1)
+
+buttons = [
+    ('P/2MASS/color', '2MASS'),
+    ('P/DSS2/color', 'DSS')
+    ]
+
+
+a.add_survey_buttons(buttons)
+
+hips_id = 'CDS/P/JWST/Southern-Ring-Nebula/NIRCam'
+hips_name = 'Southern-Ring-Nebula'
+hips_base_url = 'https://alasky.cds.unistra.fr/JWST/CDS_P_JWST_Southern-Ring-Nebula_NIRCam'
+hips_max_ord = 14
+
+a.add_image_overlayer(hips_id, hips_name, hips_base_url, hips_max_ord)
+
+a.create()
+a.save()
--- aladin.py
+++ aladin.py
@@ -1,81 +1,75 @@
 #https://aladin.cds.unistra.fr/AladinLite/doc/tutorials/interactive-finding-chart/
 
-
-keywords = ['@BS_TARGET', '@BS_FOV', '@BS_SURVEY', '@BS_WIDTH',
-            '@BS_HEIGHT', '@BS_SURBTNS', '@BS_MARKERS',
-            '@BS_SIMBAD', '@BS_VIZIER', '@BS_EPOIMG', '@BS_IMGOVLYR']
+begin = """
+<link rel="stylesheet" href="http://aladin.u-strasbg.fr/AladinLite/api/v2/latest/aladin.min.css" />
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.9.1.min.js" charset="utf-8"></script>
+\n"""
 
 class Aladin:
     def __init__(self, target, fov=1, survey="P/DSS2/color", width=700, height=700):
-        
-        with open('template.txt', 'r') as f:
-            self.html = f.read()
 
         self.target = target
         self.fov = fov
         self.survey = survey
         self.width = width
         self.height = height
-        self.buttons = None
-        self.markers = None
-        self.__simbad = None
-        self.__vizier = None
-        self.__image = None
-        self.__imgovlyr = None
+        self.html = None
+        
+        self.h1 = begin + \
+            f'<div id="aladin-lite-div" style="width:{width}px;height:{height}px;"></div>\n'
+        self.h999 = '\n<script type="text/javascript" src="http://aladin.u-strasbg.fr/AladinLite/api/v2/latest/aladin.min.js" charset="utf-8"></script>\n'
+
+        self.b1 = '\n<script type="text/javascript">\n' + \
+        f"""\tvar aladin = A.aladin('#aladin-lite-div', {{survey: "{survey}", fov:{fov}, target: "{target}", cooFrame: "ICRSd"}});\n\n"""
+        self.b999 = '</script>'
+        
+        self.__buttons = ''
+        self.__butfunc = ''
+        self.__markers = ''
+        self.__simbad = ''
+        self.__vizier = ''
+        #self.__image = ''
+        self.__imgovlyr = ''
 
 
     def create(self):
-        self.html = self.html.replace('@BS_TARGET', self.target)
-        self.html = self.html.replace('@BS_FOV', str(self.fov))
-        self.html = self.html.replace('@BS_SURVEY', self.survey)
-        self.html = self.html.replace('@BS_WIDTH', str(self.width))
-        self.html = self.html.replace('@BS_HEIGHT', str(self.height))
-        if self.buttons is not None:
-            self.html = self.html.replace('@BS_SURBTNS', self.buttons)
-            
-        if self.markers is not None:
-            self.html = self.html.replace('@BS_MARKERS', self.markers)
-        if self.__simbad is not None:
-            self.html = self.html.replace('@BS_SIMBAD', self.__simbad)
-        if self.__vizier is not None:
-            self.html = self.html.replace('@BS_VIZIER', self.__vizier)
-        if self.__image is not None:
-            self.html = self.html.replace('@BS_EPOIMG', self.__image)
-        if self.__imgovlyr is not None:
-            self.html = self.html.replace('@BS_IMGOVLYR', self.__imgovlyr)
+        self.html = self.h1 + self.__buttons + self.h999 + \
+                    self.b1 + self.__butfunc + \
+                    self.__markers + self.__simbad + self.__vizier + self.__imgovlyr + \
+                    self.b999
 
     def __add_survey_btn(self, value, id, label):
         return f'<input id="{id}" type="radio" name="survey" value="{value}"><label for="{id}">{label}<label>\n'
 
     def add_survey_buttons(self, buttons):
-        self.buttons = ''
+        self.__buttons = '\n'
         for i in range(len(buttons)):
-            self.buttons = self.buttons + self.__add_survey_btn(buttons[i][0], 'id'+str(i+1), buttons[i][1])
-        self.create()
+            self.__buttons = self.__buttons + self.__add_survey_btn(buttons[i][0], 'id'+str(i+1), buttons[i][1])
+        self.__butfunc = "\n\t$('input[name=survey]').change(function() {\n" + \
+                         "\t\taladin.setImageSurvey($(this).val());\n" + "\t});\n\n"
         
 
     def __add_marker(self, n, ra, dec, title, desc):
-        return f"var marker{n} = A.marker({ra}, {dec}, {{popupTitle: '{title}', popupDesc: '{desc}'}});\n"
+        return f"\tvar marker{n} = A.marker({ra}, {dec}, {{popupTitle: '{title}', popupDesc: '{desc}'}});\n"
 
     def add_markers(self, markers):
-        self.markers = ''
+        self.__markers = ''
         for i in range(len(markers)):
-            self.markers = self.markers + self.__add_marker(i+1, markers[i][0], markers[i][1], markers[i][2], markers[i][3])
-        self.markers = self.markers + "var markerLayer = A.catalog({color: '#800080'});\n"
-        self.markers = self.markers + "aladin.addCatalog(markerLayer);\n"
+            self.__markers = self.__markers + self.__add_marker(i+1, markers[i][0], markers[i][1], markers[i][2], markers[i][3])
+        self.__markers = self.__markers + "\tvar markerLayer = A.catalog({color: '#800080'});\n"
+        self.__markers = self.__markers + "\taladin.addCatalog(markerLayer);\n"
         tmp = ''
         for i in range(len(markers)):
             tmp = tmp + f'marker{i+1}, '
-        self.markers = self.markers + f"markerLayer.addSources([{tmp}]);\n"
-        self.create()
+        self.markers = self.markers + f"\tmarkerLayer.addSources([{tmp}]);\n"
+        
 
     def add_simbad(self, target=None, radius=None):
         if target is None:
             target = self.target
         if radius is None:
             radius = self.fov / 3
-        self.__simbad = f"aladin.addCatalog(A.catalogFromSimbad('{target}', {radius}, {{shape: 'plus', color: '#5d5', onClick: 'showTable'}}));"
-        self.create()
+        self.__simbad = f"\n\taladin.addCatalog(A.catalogFromSimbad('{target}', {radius}, {{shape: 'plus', color: '#5d5', onClick: 'showTable'}}));"
         
 
     def add_vizier(self, table=None, target=None , radius=None):
@@ -85,22 +79,18 @@ class Aladin:
             target = self.target
         if radius is None:
             radius = self.fov / 4
-        self.__vizier = f"aladin.addCatalog(A.catalogFromVizieR('{table}', '{target}', {radius}, {{shape: 'square', sourceSize: 8, color: 'red', onClick: 'showPopup'}}));"
-        self.create()
+        self.__vizier = f"\n\taladin.addCatalog(A.catalogFromVizieR('{table}', '{target}', {radius}, {{shape: 'square', sourceSize: 8, color: 'red', onClick: 'showPopup'}}));"
 
-    def add_image(self, url):
-        self.__image = f"aladin.displayJPG('{url}');"
+##    def add_image(self, url):
+##        self.__image = f"\n\taladin.displayJPG('{url}');"
 
     def add_image_overlayer(self, hips_id, hips_name, hips_base_url, hips_max_ord, coord_frame='equatorial', format='png'):
-        tmp = 'aladin.setOverlayImageLayer(aladin.createImageSurvey('
+        tmp = '\n\taladin.setOverlayImageLayer(aladin.createImageSurvey('
         tmp = tmp + f"'{hips_id}', '{hips_name}', '{hips_base_url}', '{coord_frame}', {hips_max_ord}, {{imgFormat: '{format}'}}));\n"
-        self.__imgovlyr = tmp + 'aladin.getOverlayImageLayer().setAlpha(1.0);\n'
+        self.__imgovlyr = tmp + '\taladin.getOverlayImageLayer().setAlpha(1.0);\n'
         
 
     def save(self, filename='index.html'):
-        html = self.html
-        for i in keywords:
-            html = html.replace(i, '')
         with open(filename, 'w') as f:
-            f.write(html)
+            f.write(self.html)
 
